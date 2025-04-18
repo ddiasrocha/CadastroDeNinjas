f7aa527afd0189f18afa33e5ec50deb439a9a243
David Dias <ddiasrocha@hotmail.com>
2025-04-18 06:39:36 -0300

    Revert "Criado entidade no Banco de Dados a partir do NinjaModel"
    This reverts commit 65ec35ffea3ea760a44aa24c68424d15321df98d.

--- src/main/java/dev/java10x/cadastroDeNinjas/Controller.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Controller.java
@@ -1,12 +1,13 @@
 package dev.java10x.cadastroDeNinjas;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class NinjaController {
+public class Controller {
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
