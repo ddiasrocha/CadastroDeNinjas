7037a90abcae18397555be448aae9276732983b1
David Dias <ddiasrocha@hotmail.com>
2025-04-23 04:43:26 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,12 +2,18 @@ package dev.java10x.cadastroDeNinjas.Missoes;
 
 import dev.java10x.cadastroDeNinjas.Ninja.Controller.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor //Criar um construtor sem argumentos
+@AllArgsConstructor //Criar um construtor com todos os argumentos
+@Data   //Cria os Getters e Setters
 public class MissoesModel {
 
     @Id
@@ -19,5 +25,48 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes") //uma missão poder ter vários ninjas
     private List<NinjaModel> ninjas;
 
+    /*
+    public MissoesModel() {
+
+    }
+
+    public MissoesModel(Long id, String nome, String dificuldade, List<NinjaModel> ninjas) {
+        this.id = id;
+        this.nome = nome;
+        this.dificuldade = dificuldade;
+        this.ninjas = ninjas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(String dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+
+    public List<NinjaModel> getNinjas() {
+        return ninjas;
+    }
+
+    public void setNinjas(List<NinjaModel> ninjas) {
+        this.ninjas = ninjas;
+    }  */
 
 }
--- src/main/java/dev/java10x/cadastroDeNinjas/Ninja/Controller/NinjaModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninja/Controller/NinjaModel.java
@@ -2,10 +2,17 @@ package dev.java10x.cadastroDeNinjas.Ninja.Controller;
 
 import dev.java10x.cadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Entity //transforma uma classe em uma entidade de um Banco de Dados... JPA é Java Persistence API
 @Table(name = "tb_cadastro")
+@NoArgsConstructor //Criar um construtor sem argumentos
+@AllArgsConstructor //Criar um construtor com todos os argumentos
+@Data   //Cria os Getters e Setters
 public class NinjaModel {
 
     @Id
@@ -18,6 +25,9 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") //Foreing Key ou Chave Estrangeira
     private MissoesModel missoes;//@ManyToOne um ninja pode ter uma unica missão, e uma missão pode ter diferentes ninjas
 
+
+
+    /*
     public NinjaModel(){
 
     }
@@ -26,6 +36,30 @@ public class NinjaModel {
         this.nome = nome;
         this.email = email;
         this.idade = idade;
+    }
+
+    public NinjaModel(Long id, String nome, String email, int idade, MissoesModel missoes) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+        this.missoes = missoes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public MissoesModel getMissoes() {
+        return missoes;
+    }
+
+    public void setMissoes(MissoesModel missoes) {
+        this.missoes = missoes;
     }
 
     public String getNome() {
@@ -50,6 +84,6 @@ public class NinjaModel {
 
     public void setIdade(int idade) {
         this.idade = idade;
-    }
+    } */
 
 }
