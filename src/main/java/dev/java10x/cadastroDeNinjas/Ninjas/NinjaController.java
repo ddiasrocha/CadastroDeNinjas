210ac9c982ce92f5e4f95548106963afa7ee7e6b
David Dias <ddiasrocha@hotmail.com>
2025-05-22 10:52:10 -0300

    fix: Inclui a anotation @JasonIgnore para prevenir o erro de serialização

--- src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,16 +1,51 @@
 package dev.java10x.cadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@ResponseBody
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é a minha primeira mensagem nessa rota!";
     }
+     //Adicionar Ninja (Create)
+     @PostMapping("/criar")
+     public String criarNinja(){
+        return "Ninja Criado";
+     }
+
+     //Mostrar todos os Ninjas (Read)
+     @GetMapping("/listar")
+     public List<NinjaModel> listarNinjas(){
+        return ninjaService.listarNinjas();
+     }
+
+     //Procurar Ninja por ID (Read)
+     @GetMapping("/listarid")
+     public String mostrarTodosOsNinjasPorId(){
+        return "Ninjas Mostrados por ID";
+     }
+
+     //Alterar dados dos Ninjas (Update)
+     @PutMapping("/alterarid")
+     public String alterarNinjaPorId(){
+        return "Alterar Ninja por ID";
+     }
+
+     //Deletar Ninja (Delete)
+     @DeleteMapping("/deletarId")
+     public String deletarNinjaPorId(){
+        return "Ninja Deletado por ID";
+     }
 
 }
